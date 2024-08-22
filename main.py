92f8e1660f026d04367e367414875a48578a2342
ihsanlatheef <ihsirn@hotmail.com>
2024-08-23 00:07:06 +0100

    Added player with arrow keys to make it move

--- main.py
+++ main.py
@@ -1,15 +1,34 @@
 import pygame
 from constants import *
+from player import *
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    updateable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    dt = 0
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    Player.containers = (updateable, drawable)
+    player = Player(x, y)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill((0,0,0))
+
+        for updateable_item in updateable:
+            updateable_item.update(dt)
+
+        screen.fill("black")
+
+        for drawable_item in drawable:
+            drawable_item.draw(screen)
+            
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
